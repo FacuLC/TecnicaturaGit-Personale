ed06714704c258dddd50384ba7967d7df90b4f2f
Facundo Cano <facundocano79@gmail.com>
2022-04-19 21:33:44 -0300

    Mi Primer Commit de Git

---
+++ Java/Leccion1/Hola Mundo/src/HolaMundo.java
@@ -0,0 +1,16 @@
+
+public class HolaMundo {
+    public static void main(String[] args) { // Atajo = psvm + tab
+        System.out.println("Hola Mundo desde Java"); // sout + tab
+    int miVariable = 10;
+        System.out.println(miVariable); //Atajo Ctrl + Espacio
+    //Reutilizar la Variable
+    miVariable = 5;
+        System.out.println(miVariable);
+    //Tipo String
+    String miVariableCadena = "Bienvenidos"; 
+            System.out.println(miVariableCadena);
+    miVariableCadena = "Sigamos Creciendo en Programacion";
+        System.out.println(miVariableCadena); //Ctrl + Click Izq = Origen de la Variable
+    }
+}
---
+++ Python/Leccion1/main.py
@@ -0,0 +1,14 @@
+miVariable = 3
+print(miVariable)
+miVariable = "Hola a todos los student de la tecnicatura"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(z)
+print(id(x))
+# Las literales se escriben x=152, la variable y =896 y la variable z=216
+print(id(y))
+print(id(z))
