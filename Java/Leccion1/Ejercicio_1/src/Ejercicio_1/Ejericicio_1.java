9c16cec16de89fe465a808ef92e9ed6c9308dd3d
Facundo Cano <facundocano79@gmail.com>
2022-06-15 18:53:32 -0300

    Java Clase 6 - Ejercicio 1

--- Java/Leccion1/Ejercicio_1/src/Ejercicio_1/Ejericicio_1.java
+++ Java/Leccion1/Ejercicio_1/src/Ejercicio_1/Ejericicio_1.java
@@ -7,12 +7,20 @@ public class Ejericicio_1 {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingrese su Titulo: ");
         var titulo = entrada.nextLine();
+        System.out.println("Ingrese el ID de el libro: ");
+        var ID = Integer.parseInt(entrada.nextLine());
         System.out.println("Ingrese un Autor: ");
         var autor = entrada.nextLine();
-        System.out.println("El Libro \""+titulo+"\" fue escrito por: "+autor);
+        System.out.println("Digite el precio de el libro: ");
+        var precio = Integer.parseInt(entrada.nextLine());
+        System.out.println("El Libro: "+titulo+"\nAutor: "+autor+"\nID: "+ID);
         
-        
-        
+        if (precio>=800) {
+            System.out.println("El Envio de el Libro es Gratis!!");
+        }
+        else{
+            System.out.println("El envio de el Libro es de 500$ ");
+        }    
     }
     
 }
