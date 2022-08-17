aefda4ef39d47a82fe386fbc18b768bee73ce9c4
Facundo Cano <facundocano79@gmail.com>
2022-08-16 22:15:16 -0300

    Commit segundo semestre python

---
+++ Python/Segundo Semestre/pythonClase1/Ejercicios calse 1.py
@@ -0,0 +1,18 @@
+# Ejercicio1
+print("Rango de 0 a 10 con numeros divisibles entre 3")
+for i in range(11):
+    if i % 3 == 0:
+        print(i)
+print("")
+# Ejercicio2
+print("Crea un rango de 2 a 7")
+rango = range(2, 7)
+for i in rango :
+    print(i)
+print("")
+
+# Ejercicio 3
+print("Crea un rango del 3 al 10 con incremento de 2")
+for i in range(3, 11, 2):
+    print(i)
+
