8bdb991dd5c778357abd01198c6c914c94e8d79c
Facundo Cano <facundocano79@gmail.com>
2023-04-09 23:19:29 -0300

    comit de clase 8 de js

---
+++ JavaScript/Leccion03/let y const.js
@@ -0,0 +1,9 @@
+// Hoy en dia ya no se usa 'var'
+// Se Utiliza 'let' y 'const' 
+let nombre = 'Pedro';
+ console.log(nombre);
+
+const apellido = 'Lepes'; 
+ console.log(apellido);
+
+// apellido = 'Alfaro'; // NOSE PUEDE MODIFICAR UNA CONSTANTE!!!!
