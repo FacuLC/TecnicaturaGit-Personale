a1e0709b7682fb7ea90343521ed04a4711115cbc
FacuLC <facundocano79@gmail.com>
2023-06-07 20:55:13 -0300

    Update de Prueba

--- Python/Semestre 2/Clase 1/Clase 13/Persona_Variable_de_Clase.py
+++ Python/Semestre 2/Clase 1/Clase 13/Persona_Variable_de_Clase.py
@@ -5,6 +5,7 @@ class Persona:
         self.objeto = objeto
 
 
+
 print(Persona.variable_clase)
 
 persona1 = Persona('\nEsta es una variable de objeto')
@@ -14,3 +15,13 @@ print(persona1.variable_clase)
 persona2 = Persona('Otra Variable de Objeto')
 print(persona2.objeto)
 print(persona2.variable_clase)
+
+# Creamos otra variable de clase sin estar dentro de la calse Persona
+Persona.variable_clase2 = 'Variable de clase Nueva'
+print(Persona.variable_clase2)
+
+# Ahora podemos llamarla desde los demas objetos
+print(persona1.variable_clase2)
+print(persona2.variable_clase2)
+
+
